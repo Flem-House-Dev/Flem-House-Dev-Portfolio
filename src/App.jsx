9b1878e9da7f50856d78abcab57f1e4076c2ec9e
JFleming963 <144638343+JFleming963@users.noreply.github.com>
2024-08-02 07:27:07 -0500

    added react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
@@ -5,6 +6,9 @@ function App() {
   return (
     <>
     <Header />
+    <main>
+      <Outlet />
+    </main>
     <Footer />
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import App from './App.jsx'
+import Error from './Error.jsx'
+import Home from './Home.jsx'
+import About from './About.jsx'
+import Contact from './Contact.jsx'
+import Portfolio from './Portfolio.jsx'
+import Resume from './Resume.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/About',
+        element: <About />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+      {
+        path: '/Portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: '/Resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+<RouterProvider router={router} />
+);
