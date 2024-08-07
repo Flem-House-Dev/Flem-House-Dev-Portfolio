721162f45beb298d460e683bff1e13282004d142
JFleming963 <144638343+JFleming963@users.noreply.github.com>
2024-08-06 22:44:10 -0500

    removed home page and reassigned about page as home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,46 +2,41 @@ import { Nav, Navbar, Offcanvas } from "react-bootstrap";
 import { NavLink, useLocation } from "react-router-dom";
 
 function NavBar() {
-
   const location = useLocation();
 
   return (
-   <Navbar  expand='lg'  className="m-lg-auto" collapseOnSelect >
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    {/* <Navbar.Collapse id="basic-navbar-nav"> */}
-      <Navbar.Offcanvas placement="end"  aria-labelledby="basic-navbar-nav">
+    <Navbar expand="lg" className="m-lg-auto" collapseOnSelect>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+      <Navbar.Offcanvas placement="end" aria-labelledby="basic-navbar-nav">
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>Menu</Offcanvas.Title>
         </Offcanvas.Header>
-    <Nav variant="pills" className="" defaultActiveKey={location.pathname}>
-      <Nav.Item>
-        <Nav.Link as={NavLink} to="/" eventKey="/">Home</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link as={NavLink} to="/about" eventKey="/about">
-          About
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link as={NavLink} to="/portfolio" eventKey="/portfolio">
-          Portfolio
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link as={NavLink} to="/resume" eventKey="/resume">
-          Resume
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link as={NavLink} to="/contact" eventKey="/contact">
-          Contact
-        </Nav.Link>
-      </Nav.Item>
-    </Nav>
-    </Navbar.Offcanvas>
-    {/* </Navbar.Collapse> */}
-   </Navbar>
-  
+        <Nav variant="pills" className="" defaultActiveKey={location.pathname}>
+          
+          <Nav.Item>
+            <Nav.Link as={NavLink} to="/" eventKey="/">
+              About
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link as={NavLink} to="/portfolio" eventKey="/portfolio">
+              Portfolio
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link as={NavLink} to="/resume" eventKey="/resume">
+              Resume
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link as={NavLink} to="/contact" eventKey="/contact">
+              Contact
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Offcanvas>
+    </Navbar>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,48 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import App from './App.jsx'
-import Error from './pages/Error.jsx'
-import Home from './pages/Home.jsx'
-import About from './pages/About.jsx'
-import Contact from './pages/Contact.jsx'
-import Portfolio from './pages/Portfolio.jsx'
-import Resume from './pages/Resume.jsx'
-
+import App from "./App.jsx";
+import Error from "./pages/Error.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
+import Portfolio from "./pages/Portfolio.jsx";
+import Resume from "./pages/Resume.jsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     errorElement: <Error />,
     children: [
       {
         index: true,
-        element: <Home />,
-      },
-      {
-        path: '/About',
         element: <About />,
       },
       {
-        path: '/Contact',
+        path: "/Contact",
         element: <Contact />,
       },
       {
-        path: '/Portfolio',
+        path: "/Portfolio",
         element: <Portfolio />,
       },
       {
-        path: '/Resume',
+        path: "/Resume",
         element: <Resume />,
       },
     ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-<RouterProvider router={router} />
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
