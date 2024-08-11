9b9e1cfb8a4c90168df3ff05cb871fa311cd2994
JFleming963 <144638343+JFleming963@users.noreply.github.com>
2024-08-10 22:05:27 -0500

    updated portfolio cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,11 +2,15 @@ import Card from "react-bootstrap/Card";
 
 function Cards(props) {
   return (
-    <Card className="m-2">
+    <Card className="m-3 p-3 shadow">
       <Card.Img variant="top" src={props.img} />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text>{props.text}</Card.Text>
+        <div style={{ position: 'absolute', bottom: 10, display: 'flex'}}>
+          <Card.Link href={props.viewLink}>View</Card.Link>
+          <Card.Link href={props.githubLink}>GitHub</Card.Link>
+        </div>
       </Card.Body>
     </Card>
   );
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,38 +1,104 @@
 import Cards from "../components/Cards";
 import { Row, Container } from "react-bootstrap";
 
-const titleOne = "Project 1";
-const descriptionOne = "This was some kind of project";
+// Card data ----------------------------------------------------
+import imageOne from "../assets/bit-blog.png";
+const titleOne = "Bit-Blog";
+const descriptionOne = `This website provides a place to read the latest in web 
+  developement news, as well as share your own stories.`;
+const viewLinkOne = "https://bit-blog-web-dev.onrender.com/";
+const githubLinkOne = "https://github.com/Flem-House-Dev/Bit-Blog-Web-Dev";
 
-const titleTwo = "Project 2";
-const descriptionTwo = "Another project I worked on";
+import imageTwo from "../assets/good-notes.png";
+const titleTwo = "Good Notes";
+const descriptionTwo = `Good Notes is a simple yet useful note taking app. 
+  Taking notes can be a great 
+  way to keep track of ideas. And, this app makes it simple to keep track of them 
+  by enabling the user to enter, save, and delete them.`;
+const viewLinkTwo = "https://good-notes-13au.onrender.com/";
+const githubLinkTwo = "https://github.com/Flem-House-Dev/Good-Notes";
 
-const titleThree = "Project 3";
-const descriptionThree = "This was a project I worked on";
+import imageThree from "../assets/skycast-weather.png";
+const titleThree = "SkyCast Weather";
+const descriptionThree = `This handy weather app allows the user to see the 
+  current weather conditions of a city as well as the 5-day forcast.`;
+const viewLinkThree = "https://flem-house-dev.github.io/SkyCast-Weather-App/";
+const githubLinkThree = "https://github.com/Flem-House-Dev/SkyCast-Weather-App";
 
-const titleFour = "Project 4";
-const descriptionFour = "This was a project I worked on";
+import imageFour from "../assets/JATE.png";
+const titleFour = "JATE Text Editor";
+const descriptionFour = `JATE is a text editor that allows the user to save text 
+  or code into the indexedDB on the local web browser. It can also be installed 
+  locally as a PWA.`;
+const viewLinkFour = "https://github.com/Flem-House-Dev/JATE-Text-Editor";
+const githubLinkFour = "https://github.com/Flem-House-Dev/JATE-Text-Editor";
 
-const titleFive = "Project 5";
-const descriptionFive = "This was a project I worked on";
+import imageFive from "../assets/Dino-finder.png";
+const titleFive = "Dino Finder";
+const descriptionFive = `This blogging website allows you to log information about 
+  your favorite dinos.`;
+const viewLinkFive = "https://flem-house-dev.github.io/Dino-Finder/";
+const githubLinkFive = "https://github.com/Flem-House-Dev/Dino-Finder";
 
-const titleSix = "Project 6";
-const descriptionSix = "This was a project I worked on";
+import imageSix from "../assets/crew-roster-query.png";
+const titleSix = "Crew Roster Query";
+const descriptionSix = `This app enables users to query data about crew members 
+  of the U.S.S. Enterprise. By answering a few questions, users can view or 
+  update data for crew members, roles, and departments.`;
+const viewLinkSix = "https://github.com/Flem-House-Dev/Crew-Roster-Query";
+const githubLinkSix = "https://github.com/Flem-House-Dev/Crew-Roster-Query";
 
+// Portfolio page ------------------------------------------------
 function Portfolio() {
   return (
     <div>
       <h1>Portfolio</h1>
-      <p>My portfolio will go here...</p>
+      {/* <p>My portfolio will go here...</p> */}
 
-      <Container>
-        <Row xs={1} md={2} lg={3}  className="justify-content-center mb-3">
-          <Cards title={titleOne} text={descriptionOne} />
-          <Cards title={titleTwo} text={descriptionTwo} />
-          <Cards title={titleThree} text={descriptionThree} />
-          <Cards title={titleFour} text={descriptionFour} />
-          <Cards title={titleFive} text={descriptionFive} />
-          <Cards title={titleSix} text={descriptionSix} />
+      <Container fluid>
+        <Row xs={1} md={2} lg={3} className="justify-content-evenly mb-3">
+          <Cards
+            img={imageOne}
+            title={titleOne}
+            text={descriptionOne}
+            viewLink={viewLinkOne}
+            githubLink={githubLinkOne}
+          />
+          <Cards
+            img={imageTwo}
+            title={titleTwo}
+            text={descriptionTwo}
+            viewLink={viewLinkTwo}
+            githubLink={githubLinkTwo}
+          />
+          <Cards
+            img={imageThree}
+            title={titleThree}
+            text={descriptionThree}
+            viewLink={viewLinkThree}
+            githubLink={githubLinkThree}
+          />
+          <Cards
+            img={imageFour}
+            title={titleFour}
+            text={descriptionFour}
+            viewLink={viewLinkFour}
+            githubLink={githubLinkFour}
+          />
+          <Cards
+            img={imageFive}
+            title={titleFive}
+            text={descriptionFive}
+            viewLink={viewLinkFive}
+            githubLink={githubLinkFive}
+          />
+          <Cards
+            img={imageSix}
+            title={titleSix}
+            text={descriptionSix}
+            viewLink={viewLinkSix}
+            githubLink={githubLinkSix}
+          />
         </Row>
       </Container>
     </div>
